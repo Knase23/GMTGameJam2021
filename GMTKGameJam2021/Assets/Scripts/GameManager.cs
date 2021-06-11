ce5e3ace891b288a5611a1bec96dede68246cb67
Jesper Uddefors <jesper.uddefors.95@gmail.com>
2021-06-11 23:25:47 +0200

    Assemiblies fixing and moving around scripts

---
+++ GMTKGameJam2021/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Services;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager _singeltonInstance;
+
+    private void Awake()
+    {
+        if (_singeltonInstance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _singeltonInstance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ServiceLocator.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            ServiceLocator.GetService<IAudioService>().PlaySFX("Test");
+        }
+    }
+}
